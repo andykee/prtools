b721c43cb6fd95af25d3fd613839977b2b1a8269
Andy Kee <andykee@gmail.com>
2025-06-10 09:11:01 -0700

    Additional JAX backend overloads

--- prtools/backend/_jax.py
+++ prtools/backend/_jax.py
@@ -8,16 +8,28 @@ class Numpy(BackendLibrary):
         super().__init__(importlib.import_module('jax.numpy'))
 
     def broadcast_to(self, array, shape):
-        # jax broadcast_to expects an array input
+        # jax numpy.broadcast_to expects an array input
         array = self.module.asarray(array)
         return self.module.broadcast_to(array, shape)
+
+    def divide(self, a, b, out=None):
+        # jax.numpy.divide doesn't support the `out` parameter so we
+        # ignore it
+        return self.module.divide(a, b)
 
     def dot(self, a, b, out=None):
         # jax.numpy.dot doesn't support the `out` parameter so we ignore it
         return self.module.dot(a, b)
 
+    def floor(self, x, *args, **kwargs):
+        # jax numpy.floor expects a scalar or array input. It also doesn't
+        # support the `out` parameter
+        kwargs.pop('out', None)
+        x = self.module.asarray(x)
+        return self.module.floor(x, *args, **kwargs)
+
     def max(self, a, *args, **kwargs):
-        # jax max expects an array input
+        # jax numpy.max expects an array input
         array = self.module.asarray(a)
         return self.module.max(array, *args, **kwargs)
 
@@ -26,10 +38,10 @@ class Numpy(BackendLibrary):
         # ignore it
         return self.module.multiply(a, b)
 
-    def divide(self, a, b, out=None):
-        # jax.numpy.divide doesn't support the `out` parameter so we
-        # ignore it
-        return self.module.divide(a, b)
+    def sum(self, a, *args, **kwargs):
+        kwargs.pop('out', None)
+        a = self.module.asarray(a)
+        return self.module.sum(a, *args, **kwargs)
 
 
 class Scipy(BackendLibrary):
